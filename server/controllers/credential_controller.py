3b7c873c786f17d1b338ead995dcfb5f00cfb0b8
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-06-21 19:02:23 -0500

    Update credential controller

--- server/controllers/credential_controller.py
+++ server/controllers/credential_controller.py
@@ -23,9 +23,12 @@ def get_credentials():
 @jwt.jwt_required()
 def get_credential():
     user_id = str(jwt.current_user)
-    web = request.json["website"]
-
-    cred = credential_service.findByWebsite(user_id, web)
+    if not request.json.get('id'): 
+        err_msg = {"error": "Bad credential request params" }
+        return response(err_msg, 400)
+    
+    cred_id = request.json["id"]
+    cred = credential_service.findByCredId(user_id, cred_id)
     
     if cred == None:
         return response({"msg": "Resource not found"}, 404)
@@ -39,11 +42,10 @@ def create_credential():
     cred_req = CredentRequest(request.json)
 
     if cred_req.getObject() == None:
-        err_msg = {"error": "Bad credential resquest params" }
+        err_msg = {"error": "Bad credential request params" }
         return response(err_msg, 400)
     
-    credential = cred_req.getCredential()
-    res = credential_service.create(user_id, credential.getDocument())
+    res = credential_service.createCredential(user_id, cred_req)
 
     if not res:
         return response({"msg": "Can't resolve operation"}, 400)
@@ -54,9 +56,12 @@ def create_credential():
 @jwt.jwt_required()
 def delete_credential():
     user_id = str(jwt.current_user)
-    web = request.json["website"]
-
-    result = credential_service.delete(user_id, web)
+    if not request.json.get('id'): 
+        err_msg = {"error": "Bad credential request params" }
+        return response(err_msg, 400)
+    
+    cred_id = request.json["id"]
+    result = credential_service.delete(user_id, cred_id)
 
     if not result:
         return response({"msg": "Resource not found"}, 404)
