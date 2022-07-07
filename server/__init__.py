abe13aa9b6adbfaad265e669fe2c8e58f7f4048a
JeanleeRoy <49317699+JeanleeRoy@users.noreply.github.com>
2022-07-06 19:13:39 -0500

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -7,8 +7,6 @@ CORS(app)
 
 # Load MONGO_URI and JWT_SECRET_KEY
 app.config.from_prefixed_env()
-app.config["MONGO_URI"] = "mongodb+srv://security:5gLq2FBzxRi2hxb@datastore.4tmh1zo.mongodb.net/pass_manager?retryWrites=true&w=majority"
-app.config["JWT_SECRET_KEY"] = "9ac8702499019c4aec555a75c750a7815b69"
 
 mongo.init_app(app)
 jwt.init_app(app)
